b6f02813ce1fb1a7fa51de9071f745c6cf65bd38
Sebastien Corneau <corneausebastien09@gmail.com>
2022-11-09 11:04:52 -0500

    fix: added empty state to activity feed

--- src/components/TicketInfo/Feed.jsx
+++ src/components/TicketInfo/Feed.jsx
@@ -2,11 +2,19 @@ import * as React from "react";
 import { useParams } from "react-router-dom";
 import useSWR from "swr";
 import ActivityFeed from "../ActivityFeed";
-
+import EmptyState from "../EmptyState";
 export const Feed = () => {
   const params = useParams();
   const { data } = useSWR(`/api/tickets/${params.ticketId}/events`);
   const events = data?.events || [];
 
-  return <ActivityFeed events={events} />;
+  return (
+    <>
+      {events.lenght === 0 ? (
+        <ActivityFeed events={events} />
+      ) : (
+        <EmptyState text='No activity.' />
+      )}
+    </>
+  );
 };
